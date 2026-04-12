6d41bc5b84f2eb6a41e5bebcb00a3ae14c234f94
micro <j3kaiii@gmail.com>
2026-04-12 20:53:26 +0300

    добавил еденицы измерения для продуктов

--- lib/models/shopping_list_item/shopping_list_item.dart
+++ lib/models/shopping_list_item/shopping_list_item.dart
@@ -6,6 +6,14 @@ part 'shopping_list_item.g.dart';
 
 const _uuid = Uuid();
 
+/// Типы единиц измерения для продуктов
+enum UnitType {
+  pieces, // штуки (по умолчанию)
+  kilograms, // килограммы
+  liters, // литры
+  meters // метры
+}
+
 /// Реализация продукта, добавленного в список покупок.
 @HiveType(typeId: 2)
 class ShoppingListItem extends HiveObject {
@@ -26,7 +34,21 @@ class ShoppingListItem extends HiveObject {
   // кол-во, объем или масса...
   // например: 2 буханки хлеба или 500 грамм соли.
   @HiveField(4)
-  int quantity;
+  int? quantity;
+  // тип единицы измерения (хранится как int для Hive)
+  @HiveField(5)
+  int? _unitTypeIndex;
+
+  int get safeQuantity => quantity ?? 1;
+
+  UnitType get unitType {
+    final index = _unitTypeIndex ?? 0;
+    return (index >= 0 && index < UnitType.values.length)
+        ? UnitType.values[index]
+        : UnitType.pieces;
+  }
+
+  set unitType(UnitType value) => _unitTypeIndex = value.index;
 
   ShoppingListItem({
     String? id,
@@ -34,7 +56,9 @@ class ShoppingListItem extends HiveObject {
     required this.baseName,
     this.isPurchased = false,
     this.quantity = 1,
-  }) : id = id ?? _uuid.v4();
+    UnitType unitType = UnitType.pieces,
+  })  : id = id ?? _uuid.v4(),
+        _unitTypeIndex = unitType.index;
 
   static ShoppingListItem create(Product product, {int quantity = 1}) =>
       ShoppingListItem(
@@ -46,6 +70,7 @@ class ShoppingListItem extends HiveObject {
   ShoppingListItem copyWith({
     bool? isPurchased,
     int? quantity,
+    UnitType? unitType,
   }) {
     return ShoppingListItem(
       id: id,
@@ -53,6 +78,7 @@ class ShoppingListItem extends HiveObject {
       baseName: baseName,
       isPurchased: isPurchased ?? this.isPurchased,
       quantity: quantity ?? this.quantity,
+      unitType: unitType ?? this.unitType,
     );
   }
 }
